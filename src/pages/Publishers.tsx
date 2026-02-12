67ce6ea03007fee996fcb296607c34c1d26b98ef
Tiepdv <75802102+Tiepdv@users.noreply.github.com>
2026-02-12 13:33:12 +0100

    Update Publishers.tsx

--- src/pages/Publishers.tsx
+++ src/pages/Publishers.tsx
@@ -187,12 +187,12 @@ const Publishers = () => {
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="none">None</SelectItem>
-                  <SelectItem value="4">4 weeks</SelectItem>
-                  <SelectItem value="8">8 weeks</SelectItem>
-                  <SelectItem value="12">12 weeks</SelectItem>
-                  <SelectItem value="16">16 weeks</SelectItem>
-                  <SelectItem value="20">20 weeks</SelectItem>
-                  <SelectItem value="24">24 weeks</SelectItem>
+                  <SelectItem value="15">15</SelectItem>
+                  <SelectItem value="20">20</SelectItem>
+                  <SelectItem value="30">30</SelectItem>
+                  <SelectItem value="50">50</SelectItem>
+                  <SelectItem value="100">100</SelectItem>
+                  <SelectItem value="150">150</SelectItem>
                   <SelectItem value="custom">Custom</SelectItem>
                 </SelectContent>
               </Select>
